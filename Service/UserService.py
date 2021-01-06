c87344258ae67a2caadcc496a29c6a2a1d597c94
CappuccinoCup <CappuccinoCup@126.com>
2021-01-06 20:00:59 +0800

    refactor Service

--- Service/UserService.py
+++ Service/UserService.py
@@ -1,3 +1,8 @@
-def insert_user(cursor, username, password, name, user_type):
+from connector.connectMysql import db
+
+cursor = db.cursor()
+
+
+def insert_user(username, password, name, user_type):
     cursor.execute("insert into user (username, password, name, user_type)"
                    " values ('%s', '%s', '%s', '%s')" % (username, password, name, user_type))
