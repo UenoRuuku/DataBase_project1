9798048b7bd62a1f153a258d138bc525e7276886
UenoRuuku <18302010013@fudan.edu.cn>
2021-01-06 16:52:43 +0800

    Refactor: 删除了login app，所有前端显示全部依赖service app完成，以分离前后端

--- Entrance/Hospital_System/login/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from django.template import RequestContext
-
-
-def run_service(request):
-    context = {'hello': 'Hello World!', 'dis': "none"}
-    return render(request, 'login.html', context)
-# Create your views here.
